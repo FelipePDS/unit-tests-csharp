7734ccb6659b055b8c83df46c9c914d2c140254e
FelipePDS <felipepdasilva66@gmail.com>
2024-06-04 12:19:17 -0300

    TDD - Testes com desconto negativo

--- JornadaMilhasV3/JornadaMilhas/Model/TravelOffer.cs
+++ JornadaMilhasV3/JornadaMilhas/Model/TravelOffer.cs
@@ -26,7 +26,7 @@ namespace JornadaMilhas.Model
                 {
                     Price *= (1 - MAX_DISCOUNT);
                 }
-                else
+                else if (discount > 0)
                 {
                     Price -= discount;
                 }
