9493155dad91116006382849b7b9cd1bbcf27923
FelipePDS <felipepdasilva66@gmail.com>
2024-06-17 21:17:20 -0300

    Introdução a cobertura de testes - Stryker.Net

---
+++ JornadaMilhasV4/JornadaMilhas/Model/Validator.cs
@@ -0,0 +1,17 @@
+﻿using JornadaMilhas.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Model
+{
+    public abstract class Validator : IValidator
+    {
+        private readonly Errors errors = new();
+        public bool IsValid => errors.Count() == 0;
+        public Errors Errors => errors;
+        protected abstract void Validate();
+    }
+}
---
+++ JornadaMilhasV4/JornadaMilhas/Program.cs
@@ -0,0 +1,47 @@
+﻿using JornadaMilhas.Manager;
+using JornadaMilhas.Model;
+
+List<TravelOffer> travelOfferList = new List<TravelOffer>();
+var manager = new OfferManager(travelOfferList);
+
+manager.LoadOffers();
+
+while (true)
+{
+    ShowMenu();
+
+    Console.WriteLine("Boas vindas ao Jornada Milhas. Escolha uma opção:");
+    string option = Console.ReadLine()!;
+
+    switch (option)
+    {
+        case "1":
+            manager.RegisterOffer();
+            break;
+        case "2":
+            manager.ShowAllOffers();
+            break;
+        case "3":
+            Console.WriteLine("Ofertas com maior desconto:");
+            return;
+        case "4":
+            Console.WriteLine("Obrigada por utilizar o Jornada Milhas. Até mais!");
+            return;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+
+    Console.WriteLine("\nPressione qualquer tecla para continuar...");
+    Console.ReadKey();
+    Console.Clear();
+}
+
+static void ShowMenu()
+{
+    Console.WriteLine("-------- Painel Administrativo - Jornada Milhas --------");
+    Console.WriteLine("1. Cadastrar Ofertas");
+    Console.WriteLine("2. Mostrar Todas as Ofertas");
+    Console.WriteLine("3. Exibir maiores descontos");
+    Console.WriteLine("4. Sair");
+}
