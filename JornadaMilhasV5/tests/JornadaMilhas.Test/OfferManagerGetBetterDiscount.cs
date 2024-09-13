4ff0cdd01dcbeca14cd69a2093c7d067e899e171
fsilva_BRA102 <fsilva@tecnologiaunica.com.br>
2024-09-12 23:15:20 -0300

    Introdução ao Bogus - Cenários de testes complexos

---
+++ JornadaMilhasV5/tests/JornadaMilhas.Test/OfferManagerGetBetterDiscount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using JornadaMilhas.Manager;
+using JornadaMilhas.Model;
+
+namespace JornadaMilhas.Test
+{
+    public class OfferManagerGetBetterDiscount
+    {
+        [Fact]
+        public void ReturnsOfferNullWhenListIsEmpty()
+        {
+            var list = new List<TravelOffer>();
+            var manager = new OfferManager(list);
+            Func<TravelOffer, bool> filter = o => o.Route.Destination.Equals("São Paulo");
+
+            var offer = manager.GetBetterDiscount(filter);
+
+            Assert.Null(offer);
+        }
+
+        [Fact]
+        public void ReturnsSpecificOfferWhenDestinationSaoPauloAndDiscount40()
+        {
+            var fakerPeriod = new Faker<Period>()
+                .CustomInstantiator(f =>
+                {
+                    DateTime startDate = f.Date.Soon();
+                    return new Period(startDate, startDate.AddDays(30));
+                });
+
+            var route = new Route("Curitiba", "São Paulo");
+
+            var fakerOffer = new Faker<TravelOffer>()
+                .CustomInstantiator(f => new TravelOffer(
+                    route,
+                    fakerPeriod.Generate(),
+                    100 * f.Random.Int(1, 100)
+                ))
+                .RuleFor(o => o.Discount, f => 40)
+                .RuleFor(o => o.Active, f => true);
+
+            var chosenOffer = new TravelOffer(route, fakerPeriod.Generate(), 80)
+            {
+                Discount = 40,
+                Active = true
+            };
+
+            var inactiveOffer = new TravelOffer(route, fakerPeriod.Generate(), 70)
+            {
+                Discount = 40,
+                Active = false
+            };
+
+            var offerList = fakerOffer.Generate(200);
+            offerList.Add(chosenOffer);
+            offerList.Add(inactiveOffer);
+
+            var manager = new OfferManager(offerList);
+
+            Func<TravelOffer, bool> filter = o => o.Route.Destination.Equals("São Paulo");
+            var price = 40;
+
+            var offer = manager.GetBetterDiscount(filter);
+
+            Assert.NotNull(offer);
+            Assert.Equal(price, offer.Price, 3);
+        }
+    }
+}
---
+++ JornadaMilhasV5/tests/JornadaMilhas.Test/TravelOfferDiscount.cs
@@ -0,0 +1,60 @@
+﻿using JornadaMilhas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test
+{
+    public class TravelOfferDiscount
+    {
+        [Fact]
+        public void ReturnPriceValidWhenDiscountIsApplied()
+        {
+            Route route = new("OrigemA", "OrigemB");
+            Period period = new(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double originalPrice = 100.00;
+            double discount = 20.00;
+            double priceWithDiscount = originalPrice - discount;
+
+            TravelOffer offer = new(route, period, originalPrice);
+
+            offer.Discount = discount;
+
+            Assert.Equal(priceWithDiscount, offer.Price);
+        }
+
+        [Theory]
+        [InlineData(120.00, 30)]
+        [InlineData(100.00, 30)]
+        public void ReturnMaxDiscountWhenDiscountIsGreaterOrEqualsThanPrice(double discount, double priceWithDiscount)
+        {
+            Route route = new("OrigemA", "OrigemB");
+            Period period = new(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double originalPrice = 100.00;
+
+            TravelOffer offer = new(route, period, originalPrice);
+
+            offer.Discount = discount;
+
+            Assert.Equal(priceWithDiscount, offer.Price, 3);
+        }
+
+        [Fact]
+        public void ReturnPriceValidWhenDiscountIsNegative()
+        {
+            Route route = new("OrigemA", "OrigemB");
+            Period period = new(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double originalPrice = 100.00;
+            double discount = -35.00;
+            double priceWithDiscount = 100.00;
+
+            TravelOffer offer = new(route, period, originalPrice);
+
+            offer.Discount = discount;
+
+            Assert.Equal(priceWithDiscount, offer.Price);
+        }
+    }
+}
